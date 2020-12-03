c30357456b8e4f5d7abf4114d5c0d782c9b98b8e
kangil-kim <kangil.kim@yanolja.com>
2020-12-03 10:52:52 +0900

    disabled mysql docker

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-const db = require("./db");
+const db = require('./db');
 
+//Express 서버를 생성
 const app = express();
+
+// json 형대토 오는 요청의 본문을 해석해줄수있게 등록
 app.use(bodyParser.json());
 
 // 테이블 생성하기
@@ -12,7 +15,7 @@ db.pool.query(`CREATE TABLE lists (
     value TEXT, 
     PRIMARY KEY (id)
 )`, (err, results, fileds) => {
-    if (err) console.error(err);
+    if (err) console.log(err);
     else console.log('results', results)
 })
 
@@ -21,7 +24,8 @@ db.pool.query(`CREATE TABLE lists (
 app.get('/api/hi', function (req, res) {
     //데이테베이스에서 모든 정보 가져오기
     res.status(200).send('good')
-});
+})
+
 
 
 //DB lists 테이블에 있는 모든 데이터를 프론트 서베에 보내주기
@@ -34,7 +38,7 @@ app.get('/api/values', function (req, res) {
             else
                 return res.json(results)
         })
-});
+})
 
 // 클라이언트에서 입력한 값을 데이터베이스 lists 테이블에 넣어주기
 app.post('/api/value', function (req, res, next) {
@@ -46,8 +50,8 @@ app.post('/api/value', function (req, res, next) {
             else
                 return res.json({ success: true, value: req.body.value })
         })
-});
+})
 
 app.listen(5000, () => {
-    console.log('Server is Running on port: 5000')
-});
+    console.log('애플리케이션이 5000번 포트에서 시작되었습니다.')
+})
